bda2ebfd6c5d71a5211c2c7c85f82a2dcb672b68
riccacocco <119678279+riccacocco@users.noreply.github.com>
2025-02-12 12:55:04 +0100

    feat: Implement syntax check for CI Server (Compilation) (#2) (#10)
    Implemented a syntax check feature for the CI Server as part of the core CI features.
    
    - Uses pylint with the --errors-only flag to perform a syntax check on python files
    - Captures errors and print failure message
    - Return "Syntax checked passed" only if not syntax errors are found
    - Added hello_world.py as a test file to validate the syntax check functionality
    
    Change on line 48 the file that should be analyzed

---
+++ compilation.py
@@ -0,0 +1,48 @@
+from os import listdir, path, walk
+import subprocess
+
+class CompilationCheck():
+    def __init__(self, file_path):
+        '''Initialization of the syntax check class'''
+        self.file_path = file_path
+        self.results = self.syntax_control()
+
+    def syntax_control(self):
+        """Conduct syntex check.
+        Return a boolean value: True if syntax check passed, False if error occurs or syntax check failed"""
+        print(f"Checking syntax for {self.file_path}...")
+
+        #check if the directory exists
+        if not path.exists(self.file_path):
+            print("Error: file not exist")
+            return False
+        
+        '''The function subprocess.run() is used to run the pylint command as they were runned in the terminal.
+        pylint is a tool that checks for errors in Python code. The --errors-only flag is used to only show errors and not warnings.
+        The capture_output flag is used to capture the output of the command. The text flag is used to return the output as a string.'''
+        try:
+            results = subprocess.run(
+                ["pylint","--errors-only", self.file_path], capture_output=True, text=True)
+            
+        #if pylint is not found, print an error message and return False
+        except FileNotFoundError:
+            print("Error: pylint not found")
+            return False
+        
+        
+        #Check output of pylint
+        """Check the following cases:
+        If the return code is not 0, print "Error: pylint failed" and return False.
+        Otherwise, print "Syntax check passed" and return True."""
+
+        if results.returncode != 0:
+            print("Error: pylint failed, Syntax Error detected")
+            return False
+        else:
+            print("Syntax check passed")
+            return True
+        
+
+
+if __name__ == "__main__":
+    syntax_checker = CompilationCheck("hello_world.py")
