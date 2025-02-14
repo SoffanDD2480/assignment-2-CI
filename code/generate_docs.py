afd43e02de59f3c5f794cacd707438ff8746aa76
riccacocco <r.cocco1@campus.unimib.it>
2025-02-14 10:45:48 +0100

    test: debugging

--- code/generate_docs.py
+++ code/generate_docs.py
@@ -72,4 +72,4 @@ def generate_docs(logging):
         if e.stderr:
             logging.error("Make html errors:\n%s", e.stderr)
         return
-# Test final
+# Test final -2
--- code/git_helpers.py
+++ code/git_helpers.py
@@ -74,4 +74,4 @@ def filter_files(data):
     print(changed_code_files)
     return changed_code_files
 
-# test 17687
+# test final -2
