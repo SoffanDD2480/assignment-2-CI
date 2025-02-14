c927babc084ece54cac56c3fab526ef1473037aa
riccacocco <r.cocco1@campus.unimib.it>
2025-02-14 10:52:29 +0100

    test: debugging

--- code/build_db.py
+++ code/build_db.py
@@ -80,4 +80,4 @@ def init_db(app):
     db.init_app(app)
     with app.app_context():
         db.create_all()
-# test again final - 2
+# test again final - 3
--- code/generate_docs.py
+++ code/generate_docs.py
@@ -72,4 +72,4 @@ def generate_docs(logging):
         if e.stderr:
             logging.error("Make html errors:\n%s", e.stderr)
         return
-# Test final -2
+# Test final -3
